7f7f881ebba6cd9b1ed0a7ebc595e71f002155fe
SergeA <ronnieo@gmail.com>
2018-07-26 15:37:21 -0400

    Build-it-bigger: Additional steps - added interstitial ads

--- FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -2,12 +2,17 @@ package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 
 /**
@@ -15,22 +20,58 @@ import com.google.android.gms.ads.AdView;
  */
 public class MainActivityFragment extends Fragment {
 
+    public static final String TAG = MainActivityFragment.class.getSimpleName();
+
+    private InterstitialAd mInterstitialAd;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        final View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        //AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        Button tellJokeButton = root.findViewById(R.id.tell_joke_btn);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+
+        MobileAds.initialize(getContext(), getString(R.string.mobile_service_ad_id));
+
+        final MainActivity mainActivity = (MainActivity) getActivity();
+
+        mInterstitialAd = new InterstitialAd(getContext());
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        tellJokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    mainActivity.tellJokeAndroidLib(root);
+                }
+            }
+        });
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                mainActivity.tellJokeAndroidLib(root);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
+/*
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+        */
         return root;
     }
 }
